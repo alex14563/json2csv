29eaede9e44dfb21aa5fd6c9d62549d1147f9582
LAPTOP-4QUP962N\zhangjqx <alex_14563@outlook.com>
2022-08-19 17:50:04 +0800

    rtyrty

--- src/main/java/com/game/json2csv/controller/TestController.java
+++ src/main/java/com/game/json2csv/controller/TestController.java
@@ -76,7 +76,8 @@ public class TestController {
 //        String jsonString = "[{\"__language\":\"zh\",\"__event_name\":\"applaunch\",\"__event_time\":1622722556,\"__fid\":\"1400426014731014144\",\"__oaid\":\"\",\"__useragent\":\"Mozilla/5.0 (Linux; Android 9; SM-N960U Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.210 Mobile Safari/537.36\",\"sign\":\"c720e3bffd0b972035e9777b5c3a9b53\",\"__lifetime_session_id\":\"2\",\"__appid\":\"v6jk3jdgf4zavy26fpkglwje\",\"__network_type\":\"wifi\",\"__gaid\":\"\",\"__reg\":\"cn\",\"__current_version\":\"2.0\",\"__sdk_send_time\":1622722576,\"__first_start_time\":\"1622722555\",\"__zone\":\"GMT+8\",\"__session_id\":\"2\",\"__network_name\":\"\",\"__device_vender\":\"samsung\",\"__environment\":\"0\",\"__dpi_h\":\"2094\",\"__device_name\":\"SM-N960U\",\"__pkg_name\":\"com.wuyr.catchpiggy\",\"__none_id\":\"9d201321-39dc-4e83-98d1-208f6c4bc3ef\",\"props\":{\"launch_count\":1,\"continuous_days\":1,\"day_login_count\":1,\"cur_life\":5},\"__platform\":\"1\",\"__activite_days\":\"1\",\"__device_type\":\"phone\",\"__dpi_w\":\"1080\",\"__did\":\"bbb4ae34-4904-4819-90a7-0a09f1941220\",\"__os_version\":\"9\",\"__third_party_id\":\"\",\"__bid\":\"\"}]";
         JSONArray jsonArray = new JSONArray(jsonString);
         /**
-         * testsdfsdf
+         * testsdfsdfsdf
+         * fsfsdf
          */
         return jsonArray;
     }
