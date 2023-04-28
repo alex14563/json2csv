603c44485b9f7c099034274c7e129c98688769ef
LAPTOP-4QUP962N\zhangjqx <alex_14563@outlook.com>
2023-04-28 10:57:38 +0800

    修改

--- src/main/java/com/game/json2csv/Json2csvApplication.java
+++ src/main/java/com/game/json2csv/Json2csvApplication.java
@@ -3,6 +3,9 @@ package com.game.json2csv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zhangjqx
+ */
 @SpringBootApplication
 public class Json2csvApplication {
 
